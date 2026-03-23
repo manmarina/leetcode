8a9ecf55b9b03a0499cf7c26a9aa26e39395cbad
manmarina <manmarina@me.com>
2026-03-23 13:17:51 +0900

    0088 merge-backward first

--- 0088-merge-sorted-array/main.c
+++ 0088-merge-sorted-array/main.c
@@ -5,18 +5,18 @@ void	merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size,
 
 int	main(void)
 {
-	// int nums1[6] = {1, 2, 3, 0, 0, 0};
-	// int nums2[3] = {2, 5, 6};
-	// int nums1Size = 6;
-	// int nums2Size = 3;
-	// int m = 3;
-	// int n = 3;
-	int nums1[1] = {0};
-	int nums2[1] = {1};
-	int nums1Size = 1;
-	int nums2Size = 1;
-	int m = 0;
-	int n = 1;
+	int nums1[6] = {1, 2, 3, 0, 0, 0};
+	int nums2[3] = {2, 5, 6};
+	int nums1Size = 6;
+	int nums2Size = 3;
+	int m = 3;
+	int n = 3;
+	// int nums1[1] = {0};
+	// int nums2[1] = {1};
+	// int nums1Size = 1;
+	// int nums2Size = 1;
+	// int m = 0;
+	// int n = 1;
 
 	merge(nums1, nums1Size, m, nums2, nums2Size, n);
 	int i = 0;
