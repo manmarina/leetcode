4e1da33739d3ceedfe720d2b6bdfc239f9c02110
manmarina <manmarina@me.com>
2026-03-24 14:13:54 +0900

    0100 recursive first

---
+++ 0100-same-tree/recursive.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+
+struct				TreeNode
+{
+	int				val;
+	struct TreeNode	*left;
+	struct TreeNode	*right;
+};
+
+bool	isSameTree(struct TreeNode *p, struct TreeNode *q)
+{
+	if (!p && !q)
+		return (true);
+	if (!p || !q)
+		return (false);
+	if (p->val != q->val)
+		return (false);
+	return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+}
