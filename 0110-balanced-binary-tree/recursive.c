f29ae570dc7924ad015f91a5d0332ac2e09cf4c0
manmarina <manmarina@me.com>
2026-04-01 11:22:14 +0900

    0110 recursive first

---
+++ 0110-balanced-binary-tree/recursive.c
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+
+struct				TreeNode
+{
+	int				val;
+	struct TreeNode	*left;
+	struct TreeNode	*right;
+};
+
+static int	max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+static int	height(struct TreeNode *root)
+{
+	int	left_height;
+	int	right_height;
+
+	if (!root)
+		return (0);
+	left_height = height(root->left);
+	right_height = height(root->right);
+	return (max(left_height, right_height) + 1);
+}
+
+bool	isBalanced(struct TreeNode *root)
+{
+	int left_height;
+	int right_height;
+	int diff;
+
+	if (!root)
+		return (true);
+	left_height = height(root->left);
+	right_height = height(root->right);
+	diff = left_height - right_height;
+	if (diff < 0)
+		diff = -diff;
+	if (diff > 1)
+		return (false);
+	return (isBalanced(root->left) && isBalanced(root->right));
+}
